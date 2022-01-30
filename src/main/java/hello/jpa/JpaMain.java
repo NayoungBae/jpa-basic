28e00f1f46f436c58c6fc7aa2f3ed224a7d0145f
baenayoung <nybae2003@naver.com>
2022-01-31 05:19:34 +0900

    JPQL 사용하여 Member 테이블 전체 조회

--- src/main/java/hello/jpa/JpaMain.java
+++ src/main/java/hello/jpa/JpaMain.java
@@ -4,27 +4,41 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
+        //createEntityManagerFactory 매개변수 : persistence-unit의 name 기재
+        // persistence.xml에 적어놓은 persistence-unit 정보를 불러옴
+        // DB와 연결할 수 있는 상태로 만들어줌
         EntityManagerFactory emf =
                 Persistence.createEntityManagerFactory("hello");
+        //createEntityManager 꺼내주기
         EntityManager em = emf.createEntityManager();
 
+        //실제 동작하는 코드 작성
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
         try {
-            Member findMember = em.find(Member.class, 3L);
-            findMember.setName("JPA!");
+            //JPA는 DB 테이블 대상으로 코드를 짜지 않음.
+            //객체를 대상으로 코드를 짬
+            List<Member> selectResult = em.createQuery("select m from Member as m", Member.class)
+                    .getResultList();
+
+            for(Member member : selectResult) {
+                System.out.println("member.name = " + member.getName());
+            }
 
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
         } finally {
+            //실행 코드가 끝나면 닫아줘야함
             em.close();
         }
 
+        //어플리케이션이 끝나면 닫아줘야함
         emf.close();
 
     }
